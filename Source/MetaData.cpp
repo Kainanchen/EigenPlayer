d32af7112543dc4a2d7e1516b54a6f75899a430c
Kainan <kainan.chen.cn@gmail.com>
2014-12-14 23:54:42 +0100

    Starting metadata work

---
+++ Source/MetaData.cpp
@@ -0,0 +1,9 @@
+//
+//  MetaData.cpp
+//  EigenPlayer
+//
+//  Created by Ken Chen on 14-12-14.
+//
+//
+
+#include "MetaData.h"
---
+++ Source/MetaData.h
@@ -0,0 +1,15 @@
+//
+//  MetaData.h
+//  EigenPlayer
+//
+//  Created by Ken Chen on 14-12-14.
+//
+//
+
+#ifndef __EigenPlayer__MetaData__
+#define __EigenPlayer__MetaData__
+
+#include <stdio.h>
+#include "discid.h"
+
+#endif /* defined(__EigenPlayer__MetaData__) */
